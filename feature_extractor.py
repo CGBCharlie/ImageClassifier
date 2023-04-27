76baea742e31362250d379fe1b7f39eb84d37ae4
Charlie <carlitosgarcia2000@gmail.com>
2023-04-27 00:34:28 -0600

    v2

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,8 +1,9 @@
 import cv2 
-from PIL import Image
+from PIL import Image, ImageOps
 from collections import Counter
 import csv
 import os
+import colorsys
 
 def showImage(image) :  
     cv2.imshow("Image", image)
@@ -11,63 +12,44 @@ def showImage(image) :
 
 
 # ****************** Features ******************
-def getContrast(img):
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def mostRepeatedHSV(img):
+    val = list(img.getdata())
+    val = Counter(val)
+    mc = val.most_common(5)
+    cc = [colorsys.rgb_to_hsv(p[0][0], p[0][1], p[0][2]) for p in mc]
+    return cc
 
-    mean = cv2.mean(grayImg)[0]
-    std_dev = cv2.meanStdDev(grayImg)[1][0][0]
+def mostRepeatedRGB(img):
+    val = list(img.getdata())
+    val = Counter(val)
+    mc = val.most_common(5)
+    cc1 = [p[0][0] for p in mc]
+    cc2 = [p[0][1] for p in mc]
+    cc3 = [p[0][2] for p in mc]
+    return cc1, cc2, cc3
 
-    contrast = 1
-    if std_dev == 0:
-        if mean == 255.0:
-            contrast = 0
-    else:
-        contrast = std_dev / mean
-    return contrast
-
-def getBrightness(img):
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    mean = cv2.mean(grayImg)[0]
-    brightness = mean / 255.0
-    return brightness
-
-def getSaturation(img):
-    hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-    h, s, v = cv2.split(hsvImg)
-    mean = cv2.mean(s)[0]
-    saturation = mean / 255.0
-    return saturation
-
-def mostRepeatedColors(img):
+def mostRepeatedGrayscale(img):
+    img = ImageOps.grayscale(img)
     val = list(img.getdata())
     val = Counter(val)
     mc = val.most_common(5)
     cc = [p[0] for p in mc]
     return cc
 
-def getHue(img):
-    hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-    h, s, v = cv2.split(hsvImg)
-    mean = cv2.mean(h)[0]
-    return mean / 255.0
-
-
 # ****************** Main ******************
 def main():
     directories = ['Images/cats', 'Images/ducks', 'Images/pandas']
     e = open('features.csv', 'w')
     writer = csv.writer(e)
-    writer.writerow(['      ', 'Contrast', 'Brightness', 'Saturation', 'Hue', 'Repeated Colors'])
+    writer.writerow(['      ', 'Most Repeated Red Values', 'Most Repeated Green Values', 'Most Repeated Blue Values', 'Most Repeated HSV Values', 'Most Repeated Gray Values'])
     for directory in directories:
         for filename in sorted(os.listdir(directory), key=len):
             print(filename)
             f = os.path.join(directory, filename)
             if os.path.isfile(f):
-                image = cv2.imread(f)
-                writer.writerow(['{}'.format(f.split(".")[0]), getContrast(image), getBrightness(image), getSaturation(image), getHue(image),mostRepeatedColors(Image.open(f))])
+                image = Image.open(f)
+                r, g, b = mostRepeatedRGB(image)
+                writer.writerow(['{}'.format(f.split(".")[0]), r, g, b, mostRepeatedHSV(image), mostRepeatedGrayscale(image)])
 
 if __name__ == "__main__":
     main()
